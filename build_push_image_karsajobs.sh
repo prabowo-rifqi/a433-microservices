7634dcb701a7b109ce9e69d8a511f25a8e92fd95
prabowo-rifqi <praboworifqi@student.gunadarma.ac.id>
2025-05-04 18:30:16 +0700

    Add Image Build Script & circleci config file

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Build Docker image dengan tag "karsajobs:latest"
+docker build -t $GITHUB_USERNAME/karsajobs:latest .
+
+# Menambahkan tag baru ke image agar sesuai dengan format GitHub Container Registry (GHCR)
+docker tag $GITHUB_USERNAME/karsajobs:latest ghcr.io/$GITHUB_USERNAME/a433-microservices/karsajobs:latest
+
+# Login ke GitHub Container Registry menggunakan Personal Access Token (PAT)
+# - $CR_PAT adalah variabel yang berisi Personal Access Token (harus diekspor sebelumnya)
+# - $GITHUB_USERNAME adalah username GitHub
+echo $CR_PAT | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+
+# Push image ke GitHub Container Registry (GHCR)
+docker push ghcr.io/$GITHUB_USERNAME/a433-microservices/karsajobs:latest
